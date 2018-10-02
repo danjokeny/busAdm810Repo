3b271a18fcacdb7dd3a2635f1c114f244a750b69
danjokeny <36682157+danjokeny@users.noreply.github.com>
2018-10-02 08:31:26 -0500

    clean up
    cleaned up old files in license.md directory

--- LICENSE.md/LICENSE.md
+++
@@ -1,1 +0,0 @@
-//Files for busAdm810
--- LICENSE.md/osModule.js
+++
@@ -1,7 +0,0 @@
-//OS module for operating system
-
-var os = require('os');
-console.log("hostname=",os.hostname());
-console.log("type=",os.type());
-console.log(os.platform());
-console.log((os.arch()));
