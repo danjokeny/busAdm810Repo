999b3149618b09ff7a0f4e0e9837a29f7a94b4ab
danjokeny <36682157+danjokeny@users.noreply.github.com>
2018-10-02 08:27:55 -0500

    Serving Files
    played with serving files

---
+++ servingFilesErr.js
@@ -0,0 +1,22 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+var ROOT_DIR = "html/";
+
+var port = process.env.port || 1337
+http.createServer(function (req, res) {
+    var urlObj = url.parse(req.url, true, false);
+    fs.readFile(ROOT_DIR + urlObj.pathname, function (err, data) {
+        if (err) {
+            console.log('Error Found');
+            res.writeHead(404);
+            res.end(JSON.stringify(err));
+            return;
+        }
+        console.log('No Error');
+        res.writeHead(200);
+        res.end(data);
+    });
+}).listen(port);
+console.log('Server running at http://127.0.0.1:1337/');
