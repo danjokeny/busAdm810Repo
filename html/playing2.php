359f6d6a1ed9c3c145642e81fe14c7cb6075052b
danjokeny <36682157+danjokeny@users.noreply.github.com>
2018-10-03 10:18:35 -0500

    updated php file
    updated php file

--- html/playing2.php
+++ html/playing2.php
@@ -1,4 +1,12 @@
-<?php
-// PHP code goes here
-echo "Hello World!";
-?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>What Can JavaScript Do?</h2>
+
+<p id="demo">JavaScript can change HTML content.</p>
+
+<button type="button" onclick='document.getElementById("demo").innerHTML = "Hello JavaScript!"'>Click Me!</button>
+
+</body>
+</html>
